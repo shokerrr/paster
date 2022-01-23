aca8b4b8406e16b860d614e006f36990e338cf6a
shokerrr <rijikov.shock@gmail.com>
2022-01-24 02:39:13 +0700

    GridView на главной

--- paster/views/site/index.php
+++ paster/views/site/index.php
@@ -1,53 +1,46 @@
 <?php
 
 /* @var $this yii\web\View */
+/* @var $searchModel */
+/* @var $dataProvider*/
 
-$this->title = 'My Yii Application';
+use app\models\User;
+use yii\bootstrap\Html;
+use yii\grid\GridView;
+
+$this->title = 'Главная';
 ?>
 <div class="site-index">
 
     <div class="jumbotron text-center bg-transparent">
-        <h1 class="display-4">Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <h1 class="display-4">Добро пожаловать!</h1>
+        <?= Html::a('Создать пасту', ['/site/create']) ?>
     </div>
 
     <div class="body-content">
 
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
-        </div>
-
+    <?= GridView::widget([
+            'dataProvider' => $dataProvider,
+            'columns' => [
+                ['class' => 'yii\grid\SerialColumn'],
+                'content',
+                [
+                    'attribute' => 'create_at',
+                    'format' => 'datetime'
+                ],
+                [
+                    'attribute' => 'author_id',
+                    'value' => function ($data) {
+                        return User::findOne($data->id)->nickname;
+                    }
+                ],
+                'hash',
+                [
+                        'class' => 'yii\grid\ActionColumn',
+                        'template' => '{view}'
+                ],
+            ],
+        ]);
+    ?>
     </div>
 </div>
