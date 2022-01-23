b3dd2a0df65e84c0dcb8ccafd1ded611cca5351c
shokerrr <rijikov.shock@gmail.com>
2022-01-24 01:40:47 +0700

    fix команды

--- paster/commands/UpdateController.php
+++ paster/commands/UpdateController.php
@@ -16,13 +16,13 @@ class UpdateController extends Controller
      * @var string[]
      */
     public array $intervalList = [
-        Past::NO_TIME_LIMIT => "P0100-00-00",
+        Past::NO_TIME_LIMIT => "P0100-00-00T00:00:00",
         Past::TIME_LIMIT_10_MIN => "P0000-00-00T00:10:00",
         Past::TIME_LIMIT_1_HOUR => "P0000-00-00T01:00:00",
         Past::TIME_LIMIT_3_HOUR => "P0000-00-00T03:00:00",
-        Past::TIME_LIMIT_DAY => "P0000-00-01",
-        Past::TIME_LIMIT_WEEK => "P0000-00-07",
-        Past::TIME_LIMIT_MONTH =>  "P0000-01-00",
+        Past::TIME_LIMIT_DAY => "P0000-00-01T00:00:00",
+        Past::TIME_LIMIT_WEEK => "P0000-00-07T00:00:00",
+        Past::TIME_LIMIT_MONTH =>  "P0000-01-00T00:00:00",
     ];
 
     /**
@@ -40,10 +40,6 @@ class UpdateController extends Controller
             $dateTimeNow = new \DateTime();
 
             if ($dateTimeElement < $dateTimeNow) {
-//                echo $dateTimeNow->getTimezone()->getName() . "\n";
-//                echo $dateTimeNow->format('Y-m-d H:i:s') . "\n";
-//                echo $dateTimeElement->getTimezone()->getName() . "\n";
-//                echo $dateTimeElement->format('Y-m-d H:i:s') . "\n";
                 $element->is_active = false;
                 $element->save();
             }
